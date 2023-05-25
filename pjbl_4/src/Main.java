65e549075300b6299499436151318d73bc017399
abressam <amandabressam@gmail.com>
2023-05-25 20:24:48 -0300

    Instância de teste do cálculo de calorias na Corrida

--- pjbl_4/src/Main.java
+++ pjbl_4/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Aerobico corrida = new Corrida(90,3,80);
+
+        double caloriasCorrida = corrida.calcularCaloriasGastas();
+        System.out.println("Gasto calórico: " + caloriasCorrida + " kcal");
     }
 }
