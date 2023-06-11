0e516d719383a45ef5d5cb8f5cea5a99b29b335b
aline-mattos <abelomo95@gmail.com>
2023-06-11 17:45:26 -0300

    criando classe exercicio

---
+++ src/Exercicio.java
@@ -0,0 +1,45 @@
+public abstract class Exercicio {
+    private String id;
+    private String nome;
+    private int tempoDescanso;
+    private Boolean finalizado;
+
+    Exercicio(String id, String nome, int tempoDescanso, Boolean finalizado) {
+        this.id = id;
+        this.nome = nome;
+        this.tempoDescanso = tempoDescanso;
+        this.finalizado = finalizado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTempoDescanso() {
+        return tempoDescanso;
+    }
+
+    public void setTempoDescanso(int tempoDescanso) {
+        this.tempoDescanso = tempoDescanso;
+    }
+
+    public Boolean getFinalizado() {
+        return finalizado;
+    }
+
+    public void setFinalizado(Boolean finalizado) {
+        this.finalizado = finalizado;
+    }
+}
