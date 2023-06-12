364fdfffd18f93efd7ce21bcef9722e971ed7518
Gabriel Barros <barros.fontes@unesp.br>
2023-06-12 01:58:39 -0300

    finalizando

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
   public static void main(String[] args) {
     Pucademia.abrir(new ListaTreino().mostrar());
-    // treino.progredirExercicio("AN01", new Progresso(15.0, new Date()));
   }
 }
--- src/Progresso.java
+++ src/Progresso.java
@@ -1,10 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
 public class Progresso {
     private double peso;
     private int tempo;
